44af41249987a16079477805a95f2d4f182e099f
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2015-12-05 22:51:45 +0100

    add admin tests

---
+++ didadata/tests/test_admin.py
@@ -0,0 +1,29 @@
+import pytest
+from django.contrib import admin
+
+from didadata.admin import MetricAdmin, RecordAdmin
+from didadata.models import Metric, Record
+
+
+@pytest.mark.django_db
+class TestMetricAdmin:
+
+    def test_list_display(self, rf):
+        modeladmin = MetricAdmin(Metric, admin.site)
+        assert modeladmin.list_display == ('name',)
+
+
+@pytest.mark.django_db
+class TestRecordAdmin:
+
+    def test_list_display(self, rf):
+        modeladmin = RecordAdmin(Record, admin.site)
+        assert modeladmin.list_display == ('metric_id', 'timestamp', 'value')
+
+    def test_list_filter(self, rf):
+        modeladmin = RecordAdmin(Record, admin.site)
+        assert modeladmin.list_filter == ('metric',)
+
+    def test_date_hierarchy(self, rf):
+        modeladmin = RecordAdmin(Record, admin.site)
+        assert modeladmin.date_hierarchy == 'timestamp'
