5cd847bc97fdc7a47a27fb76151e4cd3c436d586
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2016-01-01 07:05:00 +0100

    prepare for 0.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,13 @@
 Changelog
 =========
 
+0.1.1 - 2016-01-01
+------------------
+
+* adjust setup file and add
+  localization files
+
+
 0.1.0 - TBD
 ------------------
 
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 import os
+import sys
 
 from setuptools import find_packages, setup
 
 
-VERSION = '0.1.0'
+version = '0.1.1'
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print('You probably want to also tag the version now:')
+    print('  git tag -a %s -m "version %s"' % (version, version))
+    print('  git push --tags')
+    sys.exit()
 
 
 def read(*parts):
@@ -24,11 +34,17 @@ tests_require = [
 ]
 
 
+docs_requires = [
+    'sphinx',
+    'sphinx_rtd_theme'
+]
+
+
 setup(
     name='django-didadata',
     description='A Django app to collecto numeric data.',
     long_description=read('README.rst'),
-    version=VERSION,
+    version=version,
     license='BSD',
     author='Stephan Jaekel, Benjamin Banduhn',
     author_email='steph@rdev.info',
@@ -36,6 +52,8 @@ setup(
     packages=find_packages(exclude=[
         'didadata.tests',
         'didadata.tests.factories',
+        'htmlcov',
+        'build',
     ]),
     test_suite='.',
     tests_require=tests_require,
@@ -45,8 +63,11 @@ setup(
         'django-filter>=0.11,<0.12',
     ],
     extras_require={
+        'docs': docs_requires,
         'tests': tests_require,
     },
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
