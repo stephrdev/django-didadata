6bab6160d09892f17dd03da8221433d772647da4
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2018-10-27 21:34:07 +0200

    add django admin app to installed apps

--- didadata/tests/settings.py
+++ didadata/tests/settings.py
@@ -7,6 +7,10 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+
     'didadata',
-)
+]
