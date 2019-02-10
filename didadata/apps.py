ef397597dc890a3c11e0a2e2745beb171cf1ac2a
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2019-02-10 14:53:48 +0100

    fixed typo in app config

--- didadata/apps.py
+++ didadata/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class DidadatalConfig(AppConfig):
+class DidadataConfig(AppConfig):
     name = 'didadata'
     verbose_name = _('Didadata')
